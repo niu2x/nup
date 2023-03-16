91588aead841f7476d491c0dcc776415b90494ea
niu2x <niu2x@icloud.com>
2023-03-16 13:57:56 +0800

    android

--- nup/src/android/cpp/nup_z_NupJni.cpp
+++ nup/src/android/cpp/nup_z_NupJni.cpp
@@ -1,4 +1,5 @@
 #include "nup_z_NupJni.h"
+#include "nup_type.h"
 
 extern "C" {
 JNIEXPORT void JNICALL Java_nup_z_NupJni_on_1gl_1surface_1created(
@@ -15,5 +16,8 @@ JNIEXPORT void JNICALL Java_nup_z_NupJni_on_1gl_1surface_1draw(JNIEnv*, jclass)
 {
 }
 
-jint JNI_OnLoad(JavaVM* vm, void* reserved) { return JNI_VERSION_1_4; }
+jint JNI_OnLoad(JavaVM* vm, void* reserved) { 
+nup::unused(vm);
+nup::unused(reserved);
+    return JNI_VERSION_1_4; }
 }
--- tools/build_android.sh
+++ tools/build_android.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+source /usr/local/share/nxsh/nxsh.sh;
 
 cmake -S . -DTARGET_PLATFORM=Android -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=/opt/android-sdk/ndk/21.2.6472646/build/cmake/android.toolchain.cmake -B build-android-32 
 cmake --build build-android-32 -j 4
@@ -8,7 +8,10 @@ cmake --build build-android-32 -j 4
 cmake -S . -DTARGET_PLATFORM=Android -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=/opt/android-sdk/ndk/21.2.6472646/build/cmake/android.toolchain.cmake -B build-android-64 
 cmake --build build-android-64 -j 4
 
+cp build-android-32/nup/libnup.so nup/src/android/app/src/main/jniLibs/armeabi-v7a
+cp build-android-64/nup/libnup.so nup/src/android/app/src/main/jniLibs/arm64-v8a
+
 pushd .;
-cd /home/niu2x/project/nup/nup/src/android;
+cd ./nup/src/android;
 ./gradlew :app:assembleDebug
 popd;
