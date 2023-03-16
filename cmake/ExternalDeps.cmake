001324908d71fd566c880f90af49cf0c186e7269
niu2x <niu2x@icloud.com>
2023-03-17 00:59:11 +0800

    use gitee mirror

--- cmake/ExternalDeps.cmake
+++ cmake/ExternalDeps.cmake
@@ -10,7 +10,8 @@ list(APPEND TMP_CMAKE_ARGS "-DGLFW_BUILD_EXAMPLES=OFF")
 list(APPEND TMP_CMAKE_ARGS "-DGLFW_BUILD_DOCS=OFF")
 
 ExternalProject_Add(glfw3
-    GIT_REPOSITORY  https://github.com/glfw/glfw
+    # GIT_REPOSITORY  https://github.com/glfw/glfw
+    GIT_REPOSITORY  https://gitee.com/scu319hy/glfw
     GIT_TAG         3.3.8
     GIT_PROGRESS    TRUE
     CMAKE_ARGS      ${TMP_CMAKE_ARGS}
