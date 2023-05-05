c52107fd580e14d34681f723c40dbfc3db9c249e
niu2x <niu2x@icloud.com>
2023-05-05 14:25:03 +0800

    imgui

--- nup/src/linux/nup_main.cpp
+++ nup/src/linux/nup_main.cpp
@@ -14,6 +14,10 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 class exception : public std::exception {
 public:
     exception(const std::string& msg)
@@ -111,14 +115,74 @@ int main()
     }
 
     glClearColor(0, 0, 0, 0);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+    io.ConfigFlags
+        |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+    io.ConfigFlags
+        |= ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    // ImGui::StyleColorsLight();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(native_window, true);
+    ImGui_ImplOpenGL3_Init("#version 310");
+
     while (!glfwWindowShouldClose(native_window)) {
         glfwPollEvents();
         glClear(
             GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
         nup::particle_system::system_pre_draw(system);
         nup::particle_system::system_draw(system);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!"); // Create a window called "Hello,
+                                           // world!" and append into it.
+
+            ImGui::Text(
+                "This is some useful text."); // Display some text (you can use
+                                              // a format strings too)
+
+            ImGui::SliderFloat("float", &f, 0.0f,
+                1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
+
+            if (ImGui::Button(
+                    "Button")) // Buttons return true when clicked (most widgets
+                               // return true when edited/activated)
+                counter++;
+            ImGui::SameLine();
+            ImGui::Text("counter = %d", counter);
+
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(native_window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(native_window);
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     nup::particle_system::system_destroy(system);
 
